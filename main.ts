8f9ea241ce9da7d1defeeb13dde9873eeade2c86
CrownNinja <78051348+CrownNinja@users.noreply.github.com>
2023-02-28 15:05:44 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P15, 700)
-        pins.analogWritePin(AnalogPin.P16, 700)
-        basic.pause(2000)
+        pins.analogWritePin(AnalogPin.P15, 1023)
+        pins.analogWritePin(AnalogPin.P16, 1023)
+        basic.pause(100)
         pins.digitalWritePin(DigitalPin.P16, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
     } else if (receivedNumber == 1) {
@@ -26,19 +26,23 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString == "move") {
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.analogWritePin(AnalogPin.P15, 560)
-        pins.analogWritePin(AnalogPin.P16, 560)
-        basic.pause(2000)
+        pins.analogWritePin(AnalogPin.P15, 1023)
+        pins.analogWritePin(AnalogPin.P16, 1023)
+        basic.pause(100)
         pins.digitalWritePin(DigitalPin.P16, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
     } else if (receivedString == "move two") {
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.analogWritePin(AnalogPin.P16, 560)
-        pins.analogWritePin(AnalogPin.P15, 560)
-        basic.pause(2000)
+        pins.analogWritePin(AnalogPin.P16, 1023)
+        pins.analogWritePin(AnalogPin.P15, 1023)
+        basic.pause(100)
         pins.digitalWritePin(DigitalPin.P16, 0)
         pins.digitalWritePin(DigitalPin.P15, 0)
+    } else if (receivedString == "shoot") {
+        pins.servoWritePin(AnalogPin.P13, 135)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P13, 0)
     } else {
     	
     }
@@ -48,8 +52,10 @@ radio.onReceivedValue(function (name, value) {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.analogWritePin(AnalogPin.P15, 500)
     pins.analogWritePin(AnalogPin.P16, 500)
-    basic.pause(2000)
+    basic.pause(100)
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
 })
 radio.setGroup(1)
+pins.servoWritePin(AnalogPin.P0, 180)
+pins.servoSetPulse(AnalogPin.P0, 1500)
